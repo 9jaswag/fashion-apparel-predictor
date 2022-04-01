418b6202e5381fbec68a955e54a6f6a67ebe4d2b
Chuks Opia <chuks24ng@yahoo.co.uk>
2022-04-01 09:40:59 +0100

    - Upload  image - Process image - Predict - Display label

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 import tensorflow as tf
 import keras
 import numpy as np
-# from scipy.misc import imresize
-# from tensorflow import keras
+import base64
+import io
+from PIL import Image
 
 app = Flask(__name__)
 
@@ -11,7 +12,26 @@ print(tf.__version__)
 print(keras.__version__)
 
 model = keras.models.load_model('assets/multi_fashion_apparel_image_classifier.h5')
+labels = [
+  'T-shirt/top',
+  'Trouser',
+  'Pullover',
+  'Dress',
+  'Coat',
+  'Sandal',
+  'Shirt',
+  'Sneaker',
+  'Bag',
+  'Ankle boot'
+]
 
+def prepare_image(image):
+  image = image.resize((28, 28))
+  image = keras.preprocessing.image.img_to_array(image)
+  greyscale_image = image[:, :, 0]
+  image = np.expand_dims(greyscale_image, axis=0)
+
+  return image
 
 @app.route('/')
 def index():
@@ -19,7 +39,14 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-  img_data = request.get_data()
-  # im = imresize(img_data, 28, 28)
-  # im.reshape(1, 28, 28)
-  return 'Yeah'
+  request_body = request.get_json(force = True)
+  image_data = request_body['image']
+  decoded_image = base64.b64decode(image_data)
+  image = Image.open(io.BytesIO(decoded_image))
+  prepared_image = prepare_image(image)
+  prediction = model.predict(prepared_image)
+  highest_prediction = np.argmax(prediction)
+
+  print(prediction)
+  print('predicted: ', labels[highest_prediction])
+  return jsonify({ 'prediction': labels[highest_prediction] })
